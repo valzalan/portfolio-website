28aeae2efcb6f5fcbb437cf3dbe7fddf12b6bb39
Zalan Valko <valzalan@gmail.com>
2018-07-26 21:21:36 +0200

    Added tasks for prototypeing projects page

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,41 @@ const gulp = require( "gulp" ),
       uglify = require( "gulp-uglify" ),
       babel = require( "gulp-babel" ),
       source = require( "vinyl-source-stream" ),
-      buffer = require( "vinyl-buffer"),
+      buffer = require( "vinyl-buffer" ),
       sourcemaps = require( "gulp-sourcemaps" ),
       sass = require( "gulp-sass" ),
       log = require( "gulplog" );
 
 
 gulp.task( "bundle-js", function() {
-  var b = browserify({
+  var b = browserify( {
     entries: "./src/scripts/main.js",
     debug: true
   });
 
   return b.bundle()
-    .pipe( source( "bundle.js" ))
-    .pipe( buffer())
+    .pipe( source( "bundle.js" ) )
+    .pipe( buffer() )
     .pipe( babel( {
       presets: [ "env" ]
     }))
     .pipe( sourcemaps.init( {
       loadMaps: true
     }))
-      .pipe( uglify())
-      .on( "error", log.error)
-    .pipe( sourcemaps.write( "./" ))
-    .pipe( gulp.dest( "./build/scripts/" ));
+      .pipe( uglify() )
+      .on( "error", log.error )
+    .pipe( sourcemaps.write( "./" ) )
+    .pipe( gulp.dest( "./build/scripts/" ) );
+});
+
+gulp.task( "uglify-page-generator", function() {
+
+    return gulp.src( "./src/project_pages/scripts/generate-page.js" )
+      .pipe( babel( {
+        presets: [ "env" ]
+      }))
+      .pipe( uglify() )
+      .pipe( gulp.dest( "./build/project_page/scripts/" ) );
 });
 
 gulp.task( "sass", function() {
@@ -38,17 +48,38 @@ gulp.task( "sass", function() {
     .pipe( sass( {
       outputStyle: "compressed"
     })
-      .on( "error", sass.logError ))
-    .pipe( sourcemaps.write( "./" ))
-    .pipe( gulp.dest( "./build/styles/" ));
+      .on( "error", sass.logError ) )
+    .pipe( sourcemaps.write( "./" ) )
+    .pipe( gulp.dest( "./build/styles/" ) );
 });
 
+gulp.task( "watch", [ "sass", "bundle-js", "uglify-page-generator" ], function() {
 
-gulp.task( "watch", function() {
-
-  gulp.watch( "./src/scripts/**/*.js", ["bundle-js"] );
-  gulp.watch( "./src/styles/**/*.scss", ["sass"] );
+  gulp.watch( "./src/scripts/**/*.js", [ "bundle-js" ] );
+  gulp.watch( "./src/styles/**/*.scss", [ "sass" ] );
+  gulp.watch( "./src/project_pages/scripts/generate-page.js", [ "uglify-page-generator" ] );
 });
 
+//              For prototyping
+
+gulp.task( "sass-proto", function() {
+
+  return gulp.src( "./prototype/src/styles/main.scss" )
+    .pipe( sourcemaps.init() )
+    .pipe( sass( {
+      outputStyle: "compressed"
+    })
+      .on( "error", sass.logError ) )
+    .pipe( sourcemaps.write( "./" ) )
+    .pipe( gulp.dest( "./prototype/build/styles/" ) );
+});
+
+gulp.task( "watch-proto", [ "sass-proto" ], function() {
+
+  //gulp.watch( "./prototype/src/scripts/**/*.js", [ "bundle-js-proto" ] );
+  gulp.watch( "./prototype/src/styles/**/*.scss", [ "sass-proto" ] );
+});
+
+//   Default
 
 gulp.task( "default", [ "bundle-js", "sass" ] );

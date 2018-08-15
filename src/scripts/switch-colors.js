38e861d0ef63105df8d1f3f9798e7c010230e4b5
Zalan Valko <valzalan@gmail.com>
2018-08-15 13:37:17 +0200

    Icon color responsiveness

--- src/scripts/switch-colors.js
+++ src/scripts/switch-colors.js
@@ -9,9 +9,11 @@ module.exports = function( backgroundColor ) {
   if( backgroundColor == "rgb(242, 242, 242)" ) {
 
     $( "#sectionName" ).css( "color", "black" );
+    $( "#menu-icon" ).find("rect").css( "fill", "black");
 
   } else if ( backgroundColor == "rgb(14, 11, 22)" ) {
 
     $( "#sectionName" ).css( "color", "white" );
+    $( "#menu-icon" ).find("rect").css( "fill", "white");
   }
 }

63fc8982ecb6a967d46c57efcc17ade1b261087f
Zalán Valkó <valzalan@gmail.com>
2018-08-28 21:55:46 +0200

    Fixed bug with updateBrPoints()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,61 @@
 
-const $ = require( "jquery" );
-const inlineSVG = require( "inline-svg" );
+const $ = require( "jquery" ),
+      inlineSVG = require( "inline-svg" );
+
+
+  //------      Own modules      ------
+
+const background = require( "./background.js" ),
+      toggleNav = require( "./nav.js" ),
+      navscroll = require( "./nav-scroll.js" );
+
+//TODO: Maybe pull out to a file?
+// .on("load") can't be triggered inside document.ready()
+
+let breakPoints = [];
+
+$( window ).on( "resize load" , function() {
+  console.log("updating breakpoints");
+  breakPoints = updateBrPoints();
+});
+
+$( window ).on( "scroll load", function() {
+
+  let posY = $( window ).scrollTop();
+  background( posY, breakPoints );
+});
+
+function updateBrPoints() {
+
+  let winHeight = $( window ).height(),
+      marginHeights = 0,
+      contentHeight,
+      breakPoints = [],
+      sections = [ "#hero", "#about", "#skills", "#projects" ];
+
+  for( let i = 0; i < sections.length; i++ ) {
+
+    if( i != 0 ) {
+
+      marginHeights += $( sections[ i - 1 ] ).outerHeight( true );
+    }
+
+    contentHeight = $( sections[ i ] ).outerHeight();
+
+    breakPoints.push( ( marginHeights + contentHeight ) - winHeight * 0.3 );
+  }
+  console.log(breakPoints);
+  return breakPoints;
+}
+
 
 $( document ).ready( function() {
 
 inlineSVG.init( {
-  svgSelector: 'img.svg'
+  svgSelector: "img.svg"
 }, function() {   // Everything starts as a callback of inlineSVG
 
-  console.log( 'All SVGs inlined' );
-
-  //------      Own modules      ------
-
-  const background = require( "./background.js" ),
-        toggleNav = require( "./nav.js" ),
-        navscroll = require( "./nav-scroll.js" );
-
+  console.log( "All SVGs inlined" );
 
   //------      Hide everything except hero area      ------
 
@@ -48,61 +88,16 @@ inlineSVG.init( {
     navscroll( event );
   });
 
-  let breakPoints = [];
+  $( ".skill" ).click( function(event) {
 
-  $( window ).on( "resize load" , function() {
+    let id = event.currentTarget.id;
 
-    breakPoints = updateBrPoints();
+    $("#skills").find(`#${id} + li`).toggle();
 
+    setTimeout(function() {
+      $(`#${id} + li`).find("tr").toggleClass(id);
+    }, 100);
   });
-
-  $( window ).on( "scroll", function() {
-
-    let posY = $( window ).scrollTop();
-    background( posY, breakPoints );
-  });
-
-  $( window ).on( "resize load", function() {
-
-    let winWidth = $( window ).width();
-
-    if( winWidth < 550 ) {
-
-      $( "#hero h1" ).text( "Hi! I'm Zalán.\nTech enthusiast\n&\naspiring frontend\ndeveloper." );
-
-    } else {
-
-      $( "#hero h1" ).text( "Hi! I'm Zalán.\nTech enthusiast & aspiring\nfrontend developer." );
-    }
-  });
-
-  function updateBrPoints() {
-
-    let winHeight = $( window ).height(),
-
-        marginHeights = 0,
-
-        contentHeight,
-
-        breakPoints = [],
-
-        sections = [ "#hero", "#about", "#skills", "#projects" ];
-
-    for( let i = 0; i < sections.length; i++ ) {
-
-      if( i != 0 ) {
-
-        marginHeights += $( sections[ i - 1 ] ).outerHeight( true );
-
-      }
-
-      contentHeight = $( sections[ i ] ).outerHeight();
-
-      breakPoints.push( ( marginHeights + contentHeight ) - winHeight * 0.3 );
-    }
-
-    return breakPoints;
-  }
 
 });
 });

7835fa7468610b90e8339838ea0adaca37a9051c
Zalán Valkó <valzalan@gmail.com>
2018-08-29 19:44:41 +0200

    Added mobile menu animations

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,36 +1,70 @@
+//TODO: Refactor whole file. Works, but too bloated at this point.
 
 const $ = require( "jquery" );
 
-module.exports = function() {
+module.exports = function(event) {
 
-  if( $( "#navContainer" ).css( "display" ) == "none" ) {
+  if( $( "#mobile-navbar" ).css( "display" ) == "block") {
+    // Mobile nav animation
+    $( "#navContainer" ).css( "width", "100vw" );
+    if( $( "#navContainer" ).css( "display" ) == "none" ) {
+      // show
+      $( "#navContainer" ).css( "height", "0vh" );
+      $("#nav-cross").show();
 
-    $( "#sectionName" ).css( "color", "white" );
+      $( "#navContainer" ).show().animate({
+          height: "100vh"
+        }, 300, function() {
 
-    $("#nav-cross").show();
+        $( "#navContainer > ul" ).show();
+      });
+    } else {
+      // hide
+      $( "#navContainer > ul, #nav-cross" ).hide();
 
-    $( "#navContainer" ).show().animate({
-        width: "80vw"
-      }, 300, function() {
+      $( "#navContainer" ).animate({
+          height: "1vh"
+        }, 300, function() {
 
-      $( "#navContainer > ul" ).show();
-    });
+        $( "#navContainer" ).hide();
+      });
+    }
 
   } else {
 
-    $( "#navContainer > ul, #nav-cross" ).hide();
+    $( "#navContainer" ).css( "height", "100vh" );
+    // Normal nav animation
+    if( $( "#navContainer" ).css( "display" ) == "none" ) {
 
-    $( "#navContainer" ).animate({
-        width: "1vw"
-      }, 300, function() {
+      // show
+      $( "#navContainer" ).css( "width", "0vw" );
+      $( "#sectionName" ).css( "color", "white" );
 
-      $( "#navContainer" ).hide();
+      $("#nav-cross").show();
 
-      if( $( "body" ).css( "backgroundColor" ) == "rgb(242, 242, 242)" ) {
+      $( "#navContainer" ).show().animate({
+          width: "80vw"
+        }, 300, function() {
 
-        $( "#sectionName" ).css( "color", "black" );
-      }
-    });
+        $( "#navContainer > ul" ).show();
+      });
 
+    } else {
+
+      // hide
+      $( "#navContainer > ul, #nav-cross" ).hide();
+
+      $( "#navContainer" ).animate({
+          width: "1vw"
+        }, 300, function() {
+
+        $( "#navContainer" ).hide();
+
+        if( $( "body" ).css( "backgroundColor" ) == "rgb(242, 242, 242)" ) {
+
+          $( "#sectionName" ).css( "color", "black" );
+        }
+      });
+    }
   }
 }

98f60d6cd8cf7936d3613ed43c0fa31b927201da
Zalán Valkó <valzalan@gmail.com>
2018-09-03 00:53:09 +0200

    Minor bugfix

--- src/scripts/switch-colors.js
+++ src/scripts/switch-colors.js
@@ -4,16 +4,17 @@ const $ = require( "jquery" );
 module.exports = function( backgroundColor ) {
 
 	$( "body" ).css( "backgroundColor", backgroundColor );
-	$( ".line" ).toggleClass( "black" );
 
 	if ( backgroundColor == "rgb(242, 242, 242)" ) {
 
 		$( "#sectionName" ).css( "color", "black" );
 		$( "#menu-icon" ).find( "rect" ).css( "fill", "black" );
+		$( ".line" ).addClass( "black" );
 
 	} else if ( backgroundColor == "rgb(14, 11, 22)" ) {
 
 		$( "#sectionName" ).css( "color", "white" );
 		$( "#menu-icon" ).find( "rect" ).css( "fill", "white" );
+		$( ".line" ).removeClass( "black" );
 	}
 }

79d568792e252c59d3626510d811353ae4bb6be4
Zalán Valkó <valzalan@gmail.com>
2018-09-02 19:05:41 +0200

    Updated, see: /src

--- build/server.js
+++ build/server.js
@@ -1,10 +1,15 @@
 const express = require( "express" ),
       http = require( "http" ),
       path = require( "path" ),
+      //nodemailer = require( "nodemailer" ),
       app = express(),
       server = http.Server( app );
+      //bodyParser = require('body-parser');
 
-app.set( "port", 5000 );
+const port = process.env.PORT || 3000;
+
+//app.use(bodyParser.urlencoded({extended: true}));
+
 app.use( express.static( __dirname + "/public" ));
 
 // Routing
@@ -23,8 +28,35 @@ app.get( "/todolist", function( req, res ) {
 app.get( "/resume", function( req, res ) {
   res.sendFile( path.join( __dirname, "public/pages/resume/zalan_valko_resume_2018-bw.pdf" ));
 });
+/*
+app.post('/contact', function (req, res) {
 
+  let transporter = nodemailer.createTransport({
+    service: 'SendPulse',
+    auth: {
+      user: "valzalan@gmail.com",
+      pass: "wales3734<>12"
+    }
+  });
+
+  let mailOptions = {
+    from: req.body.email,
+    to: "valzalan@gmail.com",
+    subject: "Message from valzalan.me",
+    text: req.body.message
+  };
+
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
+});
+*/
 // Starts the server.
-server.listen( 5000, function() {
+server.listen( port, function() {
   console.log( "Starting server on port 5000" );
 });

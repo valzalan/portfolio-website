6cd3ba29740c9fd46540aa12e3bba64fbfaff1bd
Zalán Valkó <valzalan@gmail.com>
2019-04-28 21:04:08 +0200

    Added mode config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require( "path" );
 
 module.exports = {
+  mode: "production",
   entry: "./src/scripts/index.js",
   output: {
     path: path.resolve( __dirname, "build/public/scripts" ),
